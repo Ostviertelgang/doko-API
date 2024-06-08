613b6868108f36d4a896c675f0a5bb04df5687a4
Valentin <valentin.bister@gmail.com>
2024-06-08 21:43:22 +0200

    improve statics endpoint

--- doko_api_app/serializers.py
+++ doko_api_app/serializers.py
@@ -9,16 +9,19 @@ class PlayerSerializer(serializers.ModelSerializer):
         fields = ['player_id', 'name']
 
 
-
 class PlayerPointsSerializer(serializers.ModelSerializer):
     #player = PlayerSerializer()
     player = serializers.SlugRelatedField(slug_field='player_id', queryset=Player.objects.all())
     game_id = serializers.SerializerMethodField()
     round_id = serializers.SerializerMethodField()
+    game_created_at = serializers.SerializerMethodField()
+    game_name = serializers.SerializerMethodField()
+    round_created_at = serializers.SerializerMethodField()
 
     class Meta:
         model = PlayerPoints
-        fields = ['player', 'points', 'game_id', 'round_id']
+        #fields = ['player', 'points', 'game_id', 'round_id']
+        fields = ['player', 'points', 'game_id', 'round_id', 'game_created_at', 'game_name', 'round_created_at']
 
     def get_game_id(self, obj):
         game = obj.games.first()
@@ -30,6 +33,18 @@ class PlayerPointsSerializer(serializers.ModelSerializer):
 
     def get_player_id(self, obj):
         return obj.player.player_id
+
+    def get_game_created_at(self, obj):
+        game = obj.games.first()
+        return game.created_at if game else None
+
+    def get_game_name(self, obj):
+        game = obj.games.first()
+        return game.game_name if game else None
+
+    def get_round_created_at(self, obj):
+        round = obj.rounds.first()
+        return round.created_at if round else None
 
 
 class GameSerializer(serializers.ModelSerializer):
@@ -58,5 +73,3 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
-
-
